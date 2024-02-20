c6b6ea2a72249d2b36765058a8e9832f837aa372
Noah <nordqvist.gaming@gmail.com>
2024-02-20 19:23:09 +0100

    create explosion scene, make rocks explode into smaller rocks

--- main/main.gd
+++ main/main.gd
@@ -21,4 +21,14 @@ func spawn_rock(size: float, position = null, velocity = null) -> void:
     rock_instance.start(position, velocity, size)
     rock_instance.screen_size = screen_size
     call_deferred("add_child", rock_instance)
-    print(position)
+    rock_instance.exploded.connect(self._on_rock_exploded)
+
+
+func _on_rock_exploded(size, radius, pos, vel) -> void:
+    if size <= 1:
+        return
+    for offset in [-1, 1]:
+        var dir =  $Player.position.direction_to(pos).orthogonal() * offset
+        var new_pos = pos + dir * radius
+        var new_vel = dir * vel.length() * 1.1
+        spawn_rock(size - 1, new_pos, new_vel)
--- rock/rock.gd
+++ rock/rock.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+signal exploded
+
 var screen_size: Vector2 = Vector2.ZERO
 var size: float
 var radius: float
@@ -24,3 +26,16 @@ func start(_position: Vector2, _velocity: Vector2, _size:) -> void:
     $CollisionShape2D.shape = shape
     linear_velocity = _velocity
     angular_velocity = randf_range(-PI, PI)
+    $Explosion.scale = Vector2.ONE * 0.75 * size
+
+
+func explode() -> void:
+    $CollisionShape2D.set_deferred("disabled", true)
+    $Sprite2D.hide()
+    $Explosion/AnimationPlayer.play("explosion")
+    $Explosion.show()
+    exploded.emit(size, radius, position, linear_velocity)
+    linear_velocity = Vector2.ZERO
+    angular_velocity = 0
+    await $Explosion/AnimationPlayer.animation_finished
+    queue_free()
